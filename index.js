9828f3d420929cb9387b4ccb55ccb2d67f87df6f
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-10-10 17:02:19 -0400

    Manager email

--- index.js
+++ index.js
@@ -1,5 +1,83 @@
+// Inquirer & File System
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // require classes
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+
+// empty array for employee objects
+const employee = [];
+
+// prompt to start team or exit
+function startApp() {
+    inquirer
+        .prompt([
+            {
+                name: 'startApp',
+                type: 'confirm',
+                message: 'Would you like to assemble a team?',
+            },
+        ])
+        .then((res, err) => {
+            if (err) console.error(err);
+            if (res.startApp) {
+                addManager();
+            } else {
+                process.exit();
+            }
+        });
+}
+
+// add info for manager
+// validation for every prompt
+function addManager() {
+    inquirer
+    .prompt ([
+        {
+            name: 'name',
+            type: 'input',
+            message: "Please enter the Manager's name:",
+            validate: (input) => {
+                if (input) {
+                    return true;
+                } else {
+                    console.log("Please enter the Manager's name");
+                    return false;
+                }
+            }
+        },
+        {
+            name: 'email',
+            type: 'iput',
+            message: "Please enter Manager's email:",
+            validate: (input) => {
+                if (input) {
+                    return true;
+                } else {
+                    console.group("Please enter Manager's email");
+                    return false;
+                }
+            }
+        },
+        {
+            name: 'id',
+            type: 'input',
+            message: "Please enter Manager's ID number:",
+            validate: (input) => {
+                if (input) {
+                    return true;
+                } else {
+                    console.log("Please enter Manager's ID number")
+                    return false;
+                }
+            }
+        },
+
+    ])
+}
+
+// Start App!
+startApp();
