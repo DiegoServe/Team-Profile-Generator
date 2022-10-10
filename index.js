5676746a3b1978f6ec0ce23e9be562a47923129b
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-10-10 17:54:53 -0400

    newMember function

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const { addListener } = require('process');
 
 // empty array for employee objects
 const employee = [];
@@ -35,72 +36,102 @@ function startApp() {
 // validation for every prompt
 function addManager() {
     inquirer
-    .prompt ([
-        {
-            name: 'name',
-            type: 'input',
-            message: "Please enter the Manager's name:",
-            validate: (input) => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log("Please enter the Manager's name");
-                    return false;
+        .prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: "Please enter the Manager's name:",
+                validate: (input) => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter the Manager's name");
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            name: 'email',
-            type: 'iput',
-            message: "Please enter Manager's email:",
-            validate: (input) => {
-                if (input) {
-                    return true;
-                } else {
-                    console.group("Please enter Manager's email");
-                    return false;
+            },
+            {
+                name: 'email',
+                type: 'iput',
+                message: "Please enter Manager's email:",
+                validate: (input) => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.group("Please enter Manager's email");
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            name: 'id',
-            type: 'input',
-            message: "Please enter Manager's ID number:",
-            validate: (input) => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log("Please enter Manager's ID number")
-                    return false;
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: "Please enter Manager's ID number:",
+                validate: (input) => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log("Please enter Manager's ID number")
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            Name:'next employee',
-            type: 'confirm',
-            message: "Would you like to add another employee?",
-        },
-    ])
+            },
+            {
+                Name: 'next employee',
+                type: 'confirm',
+                message: "Would you like to add another employee?",
+            },
+        ])
 
-    // capture respnses and create new manager
-    .then((res, err) => {
-        if (err) console.error(err);
-        const newManager = new Manager(
-            res.name,
-            res.id,
-            res.email,
-        );
-        // push new manager to employees array
-        employees.push(newManager);
+        // capture responses and create new manager
+        .then((res, err) => {
+            if (err) console.error(err);
+            const newManager = new Manager(
+                res.name,
+                res.id,
+                res.email,
+            );
+            // push new manager to employees array
+            employees.push(newManager);
 
-        if (res.nextEmp) {
-            newMember();
-        } else {
-            console.log(employees);
-            renderTeam();
-        }
-    });
+            if (res.nextEmp) {
+                newMember();
+            } else {
+                console.log(employees);
+                renderTeam();
+            }
+        });
 }
+
+// if add new member is chosen, prompt for engineer, intern or "finish" will appear
+// if finish is chosen, render to existing team
+function newMember() {
+    inquirer
+        .prompt([
+                {
+            name: 'empType',
+            type: 'list',
+            message: 'Please select member position:',
+            choices: ['Engineer', 'Intern', 'Finish Team']
+                },
+        ])
+        .then((res, err) =>{
+            if (err) console.error(err);
+            switch (res.empType) {
+                case 'Engineer':
+                    addEngineer();
+                    break;
+                    case 'Intern':
+                        addListener();
+                        break;
+                        case 'Finish Team':
+                            renderTeam();
+            }
+        });
+}
+
+
+
 
 // Start App!
 startApp();
